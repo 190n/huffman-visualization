f82d36c12c317f18cff3d190825f37f215e0374a
Ben Grant <benjamin.j.grant@gmail.com>
2021-10-24 12:30:16 -0700

    Convert utf8 string to bytes

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log('hi');
+import utf8 from 'utf8';
+
+const input = document.getElementById('input') as HTMLTextAreaElement;
+
+input.addEventListener('input', () => {
+	const encoded = utf8.encode(input.value), bytes: number[] = [];
+	for (let i = 0; i < encoded.length; i += 1) {
+		bytes.push(encoded.charCodeAt(i));
+	}
+	console.log(bytes);
+}, false);

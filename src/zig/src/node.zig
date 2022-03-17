10834d3957511618f3def3cf18dcbb854d3803b3
Ben Grant <benjamin.j.grant@gmail.com>
2022-03-16 17:17:58 -0700

    Test cases for Node

--- src/zig/src/node.zig
+++ src/zig/src/node.zig
@@ -11,10 +11,12 @@ pub const Node = extern struct {
 
 pub fn nodeCreate(alloc: Allocator, symbol: u8, frequency: u32) !*Node {
     const n = try alloc.create(Node);
-    n.left = null;
-    n.right = null;
-    n.symbol = symbol;
-    n.frequency = frequency;
+    n.* = .{
+        .left = null,
+        .right = null,
+        .symbol = symbol,
+        .frequency = frequency,
+    };
     return n;
 }
 
@@ -27,4 +29,46 @@ pub fn nodeJoin(alloc: Allocator, left: *Node, right: *Node) !*Node {
 
 pub fn nodeCompare(a: *const Node, b: *const Node) Order {
     return std.math.order(a.frequency, b.frequency);
+}
+
+const expect = std.testing.expect;
+
+test "nodeJoin" {
+    const a = try nodeCreate(std.testing.allocator, 'a', 5);
+    defer std.testing.allocator.destroy(a);
+    const b = try nodeCreate(std.testing.allocator, 'b', 6);
+    defer std.testing.allocator.destroy(b);
+    const joined = try nodeJoin(std.testing.allocator, a, b);
+    defer std.testing.allocator.destroy(joined);
+
+    try expect(std.meta.eql(a.*, .{
+        .left = null,
+        .right = null,
+        .symbol = 'a',
+        .frequency = 5,
+    }));
+    try expect(std.meta.eql(b.*, .{
+        .left = null,
+        .right = null,
+        .symbol = 'b',
+        .frequency = 6,
+    }));
+    try expect(std.meta.eql(joined.*, .{
+        .left = a,
+        .right = b,
+        .symbol = '$',
+        .frequency = 11,
+    }));
+}
+
+test "nodeCompare" {
+    const a = try nodeCreate(std.testing.allocator, 'a', 5);
+    defer std.testing.allocator.destroy(a);
+    const b = try nodeCreate(std.testing.allocator, 'b', 6);
+    defer std.testing.allocator.destroy(b);
+
+    try expect(nodeCompare(a, b) == .lt);
+    try expect(nodeCompare(b, a) == .gt);
+    try expect(nodeCompare(a, a) == .eq);
+    try expect(nodeCompare(b, b) == .eq);
 }
